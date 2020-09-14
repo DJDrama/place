281473c34df1ee06e30d572c8da05600947de452
DJDrama <putzzang@hotmail.com>
2020-09-14 17:42:59 +0900

    Login and register complete(Using Firebase Auth) NOT pushing google-services.json

--- app/src/main/java/com/place/www/ui/launcher/LauncherActivity.kt
+++ app/src/main/java/com/place/www/ui/launcher/LauncherActivity.kt
@@ -1,12 +1,20 @@
 package com.place.www.ui.launcher
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupActionBarWithNavController
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.place.www.MainActivity
 import com.place.www.R
 
 class LauncherActivity: AppCompatActivity(){
+    // https://firebase.google.com/docs/auth/android/password-auth
+    private lateinit var firebaseAuth: FirebaseAuth
     private val navController by lazy{
         (supportFragmentManager.findFragmentById(R.id.fragmentContainerView)
                 as NavHostFragment).navController
@@ -14,11 +22,24 @@ class LauncherActivity: AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_launcher);
+        firebaseAuth = FirebaseAuth.getInstance()
         setupActionBarWithNavController(navController = navController)
+    }
 
+    override fun onStart() {
+        super.onStart()
+        val currentUser = firebaseAuth.currentUser
+        updateUI(currentUser)
     }
 
     override fun onSupportNavigateUp(): Boolean {
         return navController.navigateUp()
+    }
+
+    private fun updateUI(user: FirebaseUser?){
+        user?.let{
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }
     }
 }
--- app/src/main/java/com/place/www/ui/launcher/fragments/LoginFragment.kt
+++ app/src/main/java/com/place/www/ui/launcher/fragments/LoginFragment.kt
@@ -1,18 +1,27 @@
 package com.place.www.ui.launcher.fragments
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthInvalidUserException
+import com.place.www.MainActivity
 import com.place.www.R
 import kotlinx.android.synthetic.main.fragment_login.*
 
-class LoginFragment: Fragment(R.layout.fragment_login), View.OnClickListener {
+class LoginFragment : Fragment(R.layout.fragment_login), View.OnClickListener {
+    private lateinit var firebaseAuth: FirebaseAuth
 
-
+    @SuppressLint("RestrictedApi")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        firebaseAuth = FirebaseAuth.getInstance()
         //getting activity context as AppcompatActivity in order to access
         //supportActionBar
         val act = requireActivity() as AppCompatActivity
@@ -20,20 +29,46 @@ class LoginFragment: Fragment(R.layout.fragment_login), View.OnClickListener {
         act.supportActionBar?.hide()
         //Set show hide animation to false
         act.supportActionBar?.setShowHideAnimationEnabled(false)
-
+        button_login.setOnClickListener(this)
         button_register.setOnClickListener(this)
     }
 
     override fun onClick(p0: View?) {
-        p0?.let{
-            when(it.id){
-                R.id.button_register->{
+        p0?.let {
+            when (it.id) {
+                R.id.button_login -> {
+                    login()
+                }
+                R.id.button_register -> {
                     navigateToRegisterFragment(it)
                 }
             }
         }
     }
-    private fun navigateToRegisterFragment(view: View){
+
+    private fun login() {
+        val email = edt_email.text.toString()
+        val password = edt_password.text.toString()
+        firebaseAuth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    startActivity(Intent(requireActivity(), MainActivity::class.java))
+                    requireActivity().finish()
+                } else {
+                    Log.w("LoginFragment", "signInWithEmail:failure", task.exception);
+                    when(task.exception){
+                        is FirebaseAuthInvalidUserException ->{
+                            Toast.makeText(requireContext(), "일치하는 회원이 없습니다.",
+                                Toast.LENGTH_SHORT).show();
+                        }
+                        /** TODO : Find other exceptions **/
+                    }
+                }
+            }
+    }
+
+    private fun navigateToRegisterFragment(view: View) {
         view.findNavController().navigate(R.id.action_loginFragment_to_registerFragment)
     }
 }
